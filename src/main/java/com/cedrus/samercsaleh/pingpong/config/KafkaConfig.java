dfdfa065f2b21af8f78e7b87b230b23d90b48f03
Samer Saleh <samer.saleh@cedrusco.com>
2022-04-20 14:43:48 -0400

    set up config classes

---
+++ src/main/java/com/cedrus/samercsaleh/pingpong/config/KafkaConfig.java
@@ -0,0 +1,17 @@
+package com.cedrus.samercsaleh.pingpong.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "kafka")
+
+public class KafkaConfig {
+    private String bootstrapServers;
+    private String kafkaAppId;
+    private String autoOffsetReset;
+}
